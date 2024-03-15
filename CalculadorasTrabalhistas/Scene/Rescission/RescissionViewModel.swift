f170f3783a20fae88914d5101c7762a2304ee2b1
Jadiê Oliveira <jadieoliveira00@gmail.com>
2024-03-15 13:23:57 -0300

    Add func stringFromDate

--- CalculadorasTrabalhistas/Scene/Rescission/RescissionViewModel.swift
+++ CalculadorasTrabalhistas/Scene/Rescission/RescissionViewModel.swift
@@ -1,1 +1,18 @@
+import Foundation
 
+class RescissionViewModel {
+	private(set) var calculator: Calculator?
+	
+	public func setCalculator(calculator: Calculator) { self.calculator = calculator }
+	
+	public func getTitle() -> String { calculator?.name ?? ""}
+	
+	public func stringFromDate(_ dateString: String )  {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "dd/MM/yy"
+		let test = dateFormatter.date(from: dateString)
+	print("test -> \(test)")
+		
+	}
+
+}
