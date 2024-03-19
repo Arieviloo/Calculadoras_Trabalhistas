bca388a43a2d1416cf45049189ad8f7de3e2f6ae
Jadiê Oliveira <jadieoliveira00@gmail.com>
2024-03-19 09:26:30 -0300

    Add calculate result rescission

--- CalculadorasTrabalhistas/Scene/Results/Rescission/RescissionResultViewController.swift
+++ CalculadorasTrabalhistas/Scene/Results/Rescission/RescissionResultViewController.swift
@@ -2,7 +2,26 @@ import UIKit
 
 class RescissionResultViewController: UIViewController {
 	
+	private let rescissionResultView = RescissionResultView()
+	private let rescissionResultVM = RescissionResultViewModel()
+	
+	init(calculator: Calculator) {
+		super.init(nibName: nil, bundle: nil)
+		rescissionResultVM.setCalculator(calculator: calculator)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		view.backgroundColor = .red
 	}
+
+	private func configView() {
+		title = String(localizedKey: "result")
+	}
+	
 }
+
--- CalculadorasTrabalhistas/Scene/Results/Rescission/RescissionResultViewModel.swift
+++ CalculadorasTrabalhistas/Scene/Results/Rescission/RescissionResultViewModel.swift
@@ -1,1 +1,7 @@
 
+class RescissionResultViewModel {
+	private(set) var calculator: Calculator?
+	private var resultCalculation = ResultCalculation()
+	
+	public func setCalculator(calculator: Calculator) { self.calculator = calculator }
+}
