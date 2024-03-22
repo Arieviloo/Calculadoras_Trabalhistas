6f09e32593f5be8a7395950b3606e692d124f1b9
Jadiê Oliveira <jadieoliveira00@gmail.com>
2024-03-22 10:52:19 -0300

    Add additional in the salary

--- CalculadorasTrabalhistas/Scene/Results/Rescission/RescissionResultViewModel.swift
+++ CalculadorasTrabalhistas/Scene/Results/Rescission/RescissionResultViewModel.swift
@@ -11,8 +11,11 @@ class RescissionResultViewModel {
 		guard let salary = calculator?.valueSalaryGross else { return resultCalculation }
 		guard let dateContracting = calculator?.dateContracting else { return resultCalculation }
 		guard let dateResignation = calculator?.dateResignation else { return resultCalculation }
-		let valueSalaryForDay = salary / 30
-		
+		let additionalDangerouss = salary * (calculator?.valueAdditionalDangerousness ?? 0)
+		let additionalInsalubrity = salary * (calculator?.valueLevelInsalubrity ?? 0)
+		let grossSalary = salary + additionalDangerouss + additionalInsalubrity
+		let valueSalaryForDay = grossSalary / 30
+			
 		let dateComponents = DateComponents(calendar: calendar)
 		let contractingDate = calendar.dateComponents([.day, .month, .year], from: dateContracting)
 		let resignationDate = calendar.dateComponents([.day, .month, .year], from: dateResignation)
@@ -20,15 +23,15 @@ class RescissionResultViewModel {
 		
 		//Verbs rescission
 		let balanceSalary = Double(resignationDate.day ?? 0) * valueSalaryForDay
-		let vacationProportional = vacationProportional(salary, dateContracting, dateResignation)
+		let vacationProportional = vacationProportional(grossSalary, dateContracting, dateResignation)
 		let oneThirdVacation = vacationProportional / 3
-		let thirteenthProportional = thirteenthProportional(salary, dateResignation)
+		let thirteenthProportional = thirteenthProportional(grossSalary, dateResignation)
 		//Discounts
 		let inss = balanceSalary.calculateInss()
 		let inssThirteenth = thirteenthProportional.calculateInss()
 		let irrf = balanceSalary.calculateIrrf(discount: inss, numberDependent: calculator?.valueNumberDependent ?? 0)
 		//FGTS
-		let deposited = (salary * 0.08) * Double(timeInterval.month ?? 0)
+		let deposited = (grossSalary * 0.08) * Double(timeInterval.month ?? 0)
 		let fgtsBalanceSalary = balanceSalary * 0.08
 		let fgtsThirteenthProportional = thirteenthProportional * 0.08
 		let fineFGTS = deposited * 0.4
